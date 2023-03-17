98ac3fdd1bdb554b0c8387f9f02e6e590ec55049
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2023-03-17 03:11:02 +0200

    homework4-auth

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:id", isValidId, ctrl.getById);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", isValidId, validateBody(schemas.putSchema), ctrl.updateById);
+router.put("/:id", authenticate, isValidId, validateBody(schemas.putSchema), ctrl.updateById);
 
 router.patch(
-  "/:id/favorite",
+  "/:id/favorite", authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
 );
 
-router.delete("/:id", isValidId, ctrl.deleteById);
+router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
 
 module.exports = router;
